316cb8b6d831b15e2d440e8d8bc9f087126a0cc3
Paulino Fonseca <paulinofonsecass@gmail.com>
2024-07-20 05:38:51 +0100

    initial commit

---
+++ android/app/src/main/kotlin/com/tecnoelite/org/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.tecnoelite.org.tecno_image_view
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:tecno_image_view/app/app.dart';
+import 'package:tecno_image_view/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
